5c7cbcd9505dd86dd78e5405b61125679dc0c97c
Filipx21 <filip.zajac2277@gmail.com>
2020-04-30 01:16:40 +0200

    test good add

--- src/test/java/pl/exercise/warehouse/service/ProducerServiceTest.java
+++ src/test/java/pl/exercise/warehouse/service/ProducerServiceTest.java
@@ -2,28 +2,59 @@ package pl.exercise.warehouse.service;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import pl.exercise.warehouse.model.Producer;
+import pl.exercise.warehouse.repository.ProducerRepository;
 
 import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class ProducerServiceTest {
 
+    @Mock
+    private ProducerRepository producerRepository;
+
+    @InjectMocks
+    private ProducerService producerService;
+
     @Test
-    void getAll() {
+    void shouldGetAll() throws NullPointerException{
     }
 
     @Test
-    void getById() {
+    void shouldThrowExceptionGetAll() throws NullPointerException{
     }
 
     @Test
-    void add() {
+    void shouldGetById() throws NullPointerException{
     }
 
     @Test
-    void deleteById() {
+    void shouldThrowExceptionGetById() throws NullPointerException{
+    }
+
+    @Test
+    void shouldAdd() throws NullPointerException {
+        Producer expected = prepareProducer();
+
+        when(producerRepository.save(expected)).thenReturn(expected);
+
+        Producer result = producerService.add(expected);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void shouldThrowExceptionAdd() throws NullPointerException{
+    }
+
+    @Test
+    void shouldThrowExceptionDeleteById() throws NullPointerException{
     }
 
     private Producer prepareProducer(){
