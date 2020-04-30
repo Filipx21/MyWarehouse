7e1d0b8fd08bc0bd7fdb14ddb1feb36a968a5ee1
Filipx21 <filip.zajac2277@gmail.com>
2020-05-01 00:13:18 +0200

    reformat imports

--- src/main/java/pl/exercise/warehouse/service/CategoryService.java
+++ src/main/java/pl/exercise/warehouse/service/CategoryService.java
@@ -2,7 +2,9 @@ package pl.exercise.warehouse.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import org.springframework.stereotype.Service;
+
 import pl.exercise.warehouse.model.Category;
 import pl.exercise.warehouse.repository.CategoryRepository;
 
