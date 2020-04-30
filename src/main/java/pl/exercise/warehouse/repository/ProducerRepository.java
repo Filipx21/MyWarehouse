a0781509d5deafc33e6e3e15d502997dbf34dbdd
Filipx21 <filip.zajac2277@gmail.com>
2020-05-01 00:32:02 +0200

    reformat imports

--- src/main/java/pl/exercise/warehouse/repository/ProducerRepository.java
+++ src/main/java/pl/exercise/warehouse/repository/ProducerRepository.java
@@ -2,6 +2,7 @@ package pl.exercise.warehouse.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import pl.exercise.warehouse.model.Producer;
 
 @Repository
