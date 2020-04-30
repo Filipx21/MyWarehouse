1add53c9b93d21caf6cb1e5f1e5eb88582f081fe
Filipx21 <filip.zajac2277@gmail.com>
2020-05-01 00:32:41 +0200

    reformat imports

--- src/main/java/pl/exercise/warehouse/model/Product.java
+++ src/main/java/pl/exercise/warehouse/model/Product.java
@@ -7,6 +7,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+
 import java.util.Objects;
 
 @Entity
