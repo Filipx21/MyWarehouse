e3db3b3cdc224b4766bc713b72794f078f155898
Filipx21 <filip.zajac2277@gmail.com>
2020-05-03 20:25:03 +0200

    Register category mapper

--- src/main/java/pl/exercise/warehouse/config/AppConfig.java
+++ src/main/java/pl/exercise/warehouse/config/AppConfig.java
@@ -3,6 +3,7 @@ package pl.exercise.warehouse.config;
 import org.mapstruct.factory.Mappers;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import pl.exercise.warehouse.mapper.CategoryMapper;
 import pl.exercise.warehouse.mapper.ProducerMapper;
 import pl.exercise.warehouse.mapper.ProductMapper;
 
@@ -17,5 +18,10 @@ public class AppConfig {
     @Bean
     public ProducerMapper producerMapper() {
         return Mappers.getMapper(ProducerMapper.class);
+    }
+
+    @Bean
+    public CategoryMapper categoryMapper() {
+        return Mappers.getMapper(CategoryMapper.class);
     }
 }
