e681ccb16ebdb8a513b11d2cf0d3e11c223ca06d
Filipx21 <filip.zajac2277@gmail.com>
2020-05-03 20:01:07 +0200

    impl

--- src/main/java/pl/exercise/warehouse/controller/ProducerController.java
+++ src/main/java/pl/exercise/warehouse/controller/ProducerController.java
@@ -2,8 +2,13 @@ package pl.exercise.warehouse.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.exercise.warehouse.service.ProducerService;
 
 @RestController
 @RequestMapping("/api/producer")
 public class ProducerController {
+
+    private ProducerService producerService;
+
+
 }
