b82966e3dd221a3b2ddd96deb2394ff79a7aace5
Filipx21 <filip.zajac2277@gmail.com>
2020-05-03 20:24:16 +0200

    Category mapper

--- src/main/java/pl/exercise/warehouse/mapper/CategoryMapper.java
+++ src/main/java/pl/exercise/warehouse/mapper/CategoryMapper.java
@@ -1,4 +1,21 @@
 package pl.exercise.warehouse.mapper;
 
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+import pl.exercise.warehouse.dto.CategoryDto;
+import pl.exercise.warehouse.model.Category;
+
+@Mapper
 public interface CategoryMapper {
+
+    @Mappings({
+            @Mapping(target = "id", source = "categoryDto.id"),
+            @Mapping(target = "name", source = "categoryDto.name")})
+    Category toCategory(CategoryDto categoryDto);
+
+    @Mappings({
+            @Mapping(target = "id", source = "category.id"),
+            @Mapping(target = "name", source = "category.name")})
+    CategoryDto toCategoryDto(Category category);
 }
