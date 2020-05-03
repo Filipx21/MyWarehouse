8be4a3ed30358fdcd5a4d82674c0930db5e5a428
Filipx21 <filip.zajac2277@gmail.com>
2020-05-03 20:31:08 +0200

    category controller

--- src/main/java/pl/exercise/warehouse/controller/CategoryController.java
+++ src/main/java/pl/exercise/warehouse/controller/CategoryController.java
@@ -1,12 +1,53 @@
 package pl.exercise.warehouse.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import pl.exercise.warehouse.dto.CategoryDto;
+import pl.exercise.warehouse.mapper.CategoryMapper;
+import pl.exercise.warehouse.model.Category;
 import pl.exercise.warehouse.service.CategoryService;
+
+import javax.validation.Valid;
+import java.net.URI;
 
 @RestController
 @RequestMapping("/api/category")
 public class CategoryController {
 
-    private CategoryService categoryServicel;
+    private CategoryService categoryService;
+    private CategoryMapper categoryMapper;
+
+    public CategoryController(CategoryService categoryService, CategoryMapper categoryMapper) {
+        this.categoryService = categoryService;
+        this.categoryMapper = categoryMapper;
+    }
+
+    @PostMapping("/category")
+    public ResponseEntity<CategoryDto> saveCategory(@Valid @RequestBody CategoryDto category){
+        try{
+            Category _category = categoryMapper.toCategory(category);
+            Category addedCategory = categoryService.add(_category);
+            CategoryDto categoryDto = categoryMapper.toCategoryDto(addedCategory);
+            URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                    .path("/{id}")
+                    .buildAndExpand(addedCategory.getId())
+                    .toUri();
+            return ResponseEntity.created(uri).body(categoryDto);
+        }catch(Exception ex){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @DeleteMapping("/category")
+    public ResponseEntity<Object> deleteCategory(@PathVariable("id") Long id){
+        try {
+            categoryService.deleteById(id);
+            return ResponseEntity.noContent().build();
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
 }
