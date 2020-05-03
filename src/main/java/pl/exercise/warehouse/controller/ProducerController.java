f9fae5218fefb3df07685448ce51d1cbaa3cc49c
Filipx21 <filip.zajac2277@gmail.com>
2020-05-03 19:52:27 +0200

    Producer controller

---
+++ src/main/java/pl/exercise/warehouse/controller/ProducerController.java
@@ -0,0 +1,9 @@
+package pl.exercise.warehouse.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/producer")
+public class ProducerController {
+}
