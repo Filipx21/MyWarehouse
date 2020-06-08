5d6040374664771a606e89b71ed712d41e15bd6a
Filipx21 <filip.zajac2277@gmail.com>
2020-06-08 23:38:20 +0200

    dto

--- src/main/java/pl/exercise/warehouse/dto/CategoryDto.java
+++ src/main/java/pl/exercise/warehouse/dto/CategoryDto.java
@@ -1,15 +1,43 @@
 package pl.exercise.warehouse.dto;
 
-import lombok.Getter;
-import lombok.Setter;
-
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 public class CategoryDto {
 
-    @Getter @Setter private long id;
+    private long id;
     @NotNull @Size(max = 50)
-    @Getter @Setter private String name;
+    private String name;
 
+    public CategoryDto() {
+    }
+
+    public CategoryDto(long id, @NotNull @Size(max = 50) String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "CategoryDto{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
