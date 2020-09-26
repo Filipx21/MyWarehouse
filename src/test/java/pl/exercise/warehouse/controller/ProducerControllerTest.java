c52a2b772f032a010479388bdd12ecda589ab258
Filipx21 <filip.zajac2277@gmail.com>
2020-09-26 14:32:19 +0200

    PorducerControllerTest

---
+++ src/test/java/pl/exercise/warehouse/controller/ProducerControllerTest.java
@@ -0,0 +1,15 @@
+package pl.exercise.warehouse.controller;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import pl.exercise.warehouse.controller.config.REST_TestConfig;
+
+@ExtendWith(MockitoExtension.class)
+@WebMvcTest(controllers = ProducerController.class)
+@ComponentScan(basePackageClasses = {REST_TestConfig.class})
+public class ProducerControllerTest {
+
+
+}
