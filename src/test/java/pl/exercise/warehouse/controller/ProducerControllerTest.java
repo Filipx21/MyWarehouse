1f6c9ed05af1947889d8c5b3d9fc2ebf41a94f61
Filipx21 <filip.zajac2277@gmail.com>
2020-09-26 14:43:07 +0200

    properies

--- src/test/java/pl/exercise/warehouse/controller/ProducerControllerTest.java
+++ src/test/java/pl/exercise/warehouse/controller/ProducerControllerTest.java
@@ -1,13 +1,19 @@
 package pl.exercise.warehouse.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.web.servlet.MockMvc;
 import pl.exercise.warehouse.controller.config.REST_TestConfig;
 import pl.exercise.warehouse.dto.ProducerDto;
+import pl.exercise.warehouse.mapper.ProducerMapper;
 import pl.exercise.warehouse.model.Producer;
 import pl.exercise.warehouse.model.Product;
+import pl.exercise.warehouse.service.ProducerService;
 
 import java.util.ArrayList;
 
@@ -15,6 +21,19 @@ import java.util.ArrayList;
 @WebMvcTest(controllers = ProducerController.class)
 @ComponentScan(basePackageClasses = {REST_TestConfig.class})
 public class ProducerControllerTest {
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ProducerMapper producerMapper;
+
+    @MockBean
+    private ProducerService producerService;
+
 
 
 
