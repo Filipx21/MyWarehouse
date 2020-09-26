e8c67e4806f51bfb909fc109ada84421223b3d6d
Filipx21 <filip.zajac2277@gmail.com>
2020-09-26 14:40:24 +0200

    remove lombok from dto model

--- src/main/java/pl/exercise/warehouse/dto/ProducerDto.java
+++ src/main/java/pl/exercise/warehouse/dto/ProducerDto.java
@@ -1,8 +1,5 @@
 package pl.exercise.warehouse.dto;
 
-import lombok.Getter;
-import lombok.Setter;
-
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.Size;
@@ -11,15 +8,62 @@ import java.util.List;
 
 public class ProducerDto {
 
-    @Getter @Setter private long id;
+    private long id;
     @Size(min = 2, max = 50)
-    @Getter @Setter private String companyName;
+    private String companyName;
     @Size(min = 2, max = 50)
-    @Getter @Setter private String owner;
+    private String owner;
     @Size(min = 2, max = 50)
-    @Getter @Setter private String address;
+    private String address;
     @Min(0) @Max(10)
-    @Getter @Setter private int opinion;
-    @Getter @Setter private List<ProductDto> products;
+    private int opinion;
+    private List<ProductDto> products;
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getOpinion() {
+        return opinion;
+    }
+
+    public void setOpinion(int opinion) {
+        this.opinion = opinion;
+    }
+
+    public List<ProductDto> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<ProductDto> products) {
+        this.products = products;
+    }
 }
