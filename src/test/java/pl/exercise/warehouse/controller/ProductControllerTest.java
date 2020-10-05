a5ec211c220e55c64ac8f9e16b4a9249cf8e850f
Filipx21 <filip.zajac2277@gmail.com>
2020-10-05 20:13:11 +0200

    Find product by id - return product

--- src/test/java/pl/exercise/warehouse/controller/ProductControllerTest.java
+++ src/test/java/pl/exercise/warehouse/controller/ProductControllerTest.java
@@ -9,19 +9,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import pl.exercise.warehouse.controller.config.REST_TestConfig;
 import pl.exercise.warehouse.dto.CategoryDto;
 import pl.exercise.warehouse.dto.ProducerDto;
 import pl.exercise.warehouse.dto.ProductDto;
 import pl.exercise.warehouse.mapper.ProductMapper;
+import pl.exercise.warehouse.model.Producer;
+import pl.exercise.warehouse.model.Product;
 import pl.exercise.warehouse.service.ProductService;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @DisplayName("Product Controller")
 @ExtendWith(MockitoExtension.class)
-@WebMvcTest(controllers = ProducerController.class)
+@WebMvcTest(controllers = ProductController.class)
 @ComponentScan(basePackageClasses = {REST_TestConfig.class})
-public class ProductControllerTest {
+class ProductControllerTest {
 
     @Autowired
     private ObjectMapper objectMapper;
@@ -38,7 +46,25 @@ public class ProductControllerTest {
     @Test
     @DisplayName("Find product by id - return product")
     void shouldFindProductById() throws Exception {
+        var productFromWeb = prepareProductDto();
+        productFromWeb.setId(1L);
+        var expected = mapper.toProduct(productFromWeb);
 
+        when(service.getById(productFromWeb.getId()))
+                .thenReturn(expected);
+
+        var mvcResult = mockMvc.perform(
+                get("/api/product/product/{id}", productFromWeb.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status()
+                        .isOk()
+                ).andReturn();
+
+        var jsonString = mvcResult.getResponse().getContentAsString();
+        var result = objectMapper.readValue(jsonString, Product.class);
+
+        assertEquals(expected, result);
     }
 
     @Test
