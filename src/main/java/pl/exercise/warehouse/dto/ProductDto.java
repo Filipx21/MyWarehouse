84ba5ee67ebfd4d04f20ade58aaa6c67ac5f9c33
Filipx21 <filip.zajac2277@gmail.com>
2020-10-05 20:00:07 +0200

    remove lombok from productDto

--- src/main/java/pl/exercise/warehouse/dto/ProductDto.java
+++ src/main/java/pl/exercise/warehouse/dto/ProductDto.java
@@ -1,8 +1,5 @@
 package pl.exercise.warehouse.dto;
 
-import lombok.Getter;
-import lombok.Setter;
-
 import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
@@ -11,18 +8,18 @@ import javax.validation.constraints.Size;
 
 public class ProductDto {
 
-    @Getter @Setter private long id;
+    private long id;
     @Size(min = 2, max = 50)
-    @Getter @Setter private String name;
+    private String name;
     @Min(1)
-    @Getter @Setter private int quantity;
+    private int quantity;
     @DecimalMin(value = "0.0", inclusive = false)
-    @Getter @Setter private double price;
-    @Getter @Setter private ProducerDto producer;
-    @Getter @Setter private CategoryDto category;
+    private double price;
+    private ProducerDto producer;
+    private CategoryDto category;
     @NotNull
-    @Getter @Setter private String photoUri;
+    private String photoUri;
     @Min(0) @Max(10)
-    @Getter @Setter private int opinion;
+    private int opinion;
 
 }
