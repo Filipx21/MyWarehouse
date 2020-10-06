d9d30d14ba31dee5940e0710e0bd4a93a7f1bcd7
Filipx21 <filip.zajac2277@gmail.com>
2020-10-06 20:13:17 +0200

    name

--- src/test/java/pl/exercise/warehouse/service/ProductServiceTest.java
+++ src/test/java/pl/exercise/warehouse/service/ProductServiceTest.java
@@ -19,7 +19,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
-@DisplayName("Test Product")
+@DisplayName("Product Service")
 @ExtendWith(MockitoExtension.class)
 class ProductServiceTest {
 
